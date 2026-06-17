2842cecfd04b39c8688441c87df8f4ec6fbb2b1e
youwei <a605042000@gmail.com>
2026-06-17 22:31:12 +0800

    建圖導航精度調整為1cm

--- slam_tool/launch/slam_toolbox_online_async.launch.py
+++ slam_tool/launch/slam_toolbox_online_async.launch.py
@@ -3,16 +3,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    bringup_dir = get_package_share_directory('fishbot_bringup')
+    slam_tool_dir = get_package_share_directory('slam_tool')
     slam_toolbox_dir = get_package_share_directory('slam_toolbox')
 
     use_sim_time = LaunchConfiguration('use_sim_time')
     slam_params_file = LaunchConfiguration('slam_params_file')
+    use_rviz = LaunchConfiguration('use_rviz')
+    rviz_config_file = LaunchConfiguration('rviz_config_file')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -22,8 +26,18 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             'slam_params_file',
-            default_value=os.path.join(bringup_dir, 'config', 'slam_toolbox_online_async.yaml'),
+            default_value=os.path.join(slam_tool_dir, 'config', 'slam_toolbox_online_async.yaml'),
             description='Path to slam_toolbox parameter file.'
+        ),
+        DeclareLaunchArgument(
+            'use_rviz',
+            default_value='true',
+            description='Start RViz2 with slam mapping config if true.'
+        ),
+        DeclareLaunchArgument(
+            'rviz_config_file',
+            default_value=os.path.join(slam_tool_dir, 'rviz', 'slam_toolbox_mapping.rviz'),
+            description='Path to RViz2 configuration file.'
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -33,5 +47,14 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time,
                 'slam_params_file': slam_params_file,
             }.items(),
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-d', rviz_config_file],
+            condition=IfCondition(use_rviz),
         ),
     ])
